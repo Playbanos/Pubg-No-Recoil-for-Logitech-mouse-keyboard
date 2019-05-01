a97f5959425b5c0e49674926f386b4618d57bc86
Playbanos <48029520+Playbanos@users.noreply.github.com>
2019-05-01 19:37:27 +0200

    for monitoring

---
+++ DEBUSG/Program.cs
@@ -0,0 +1,40 @@
+﻿using DbMon.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEBUSG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DebugMonitor.Start();
+            DebugMonitor.OnOutputDebugString += new
+                  OnOutputDebugStringHandler(OnOutputDebugString);
+            Console.WriteLine("Press 'Enter' to exit.");
+            Console.ReadLine();
+            DebugMonitor.Stop();
+        }
+        private static void OnOutputDebugString(int pid, string text)
+        {
+
+            if (text.Contains("LOGITECHSCRIPT_"))
+            {
+                SpeechSynthesizer synth = new SpeechSynthesizer();
+
+                // Configure the audio output.   
+                synth.SetOutputToDefaultAudioDevice();
+
+                // Speak a string.  
+                synth.Speak(text.Replace("LOGITECHSCRIPT_",""));
+
+                 Console.WriteLine(DateTime.Now + ": [" + pid + "] " + text);
+
+            }
+        }
+    }
+}
